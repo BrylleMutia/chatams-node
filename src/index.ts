d9773185da831f7f8c879f9cfb9e804b74ab4cf4
bryllemutia <bryllemutia3@gmail.com>
2024-04-30 14:35:17 +0800

    initialized middleware + mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import express, { Express, Request, Response } from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = 8000;
+const MONGO_URI = `mongodb+srv://bryllemutia:${process.env.MONGO_PASSWORD}@cluster0.qt8cm1e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // you can explicitly annotate the types, but is not required
 const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression()); // will attempt to compress response bodies for all request
+app.use(cookieParser()); // parse Cookie header and populate req.cookies with an object keyed by the cookie names
+app.use(bodyParser.json()); // extracts the entire body portion of an incoming request stream and exposes it on req.body
 
 app.get("/", (req, res) => {
    res.send("Hello World Node + Express + Typescript!!");
 });
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
    console.log(`Listening to port: ${PORT}`);
 });
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection.on("error", (error: Error) => console.log(error));
