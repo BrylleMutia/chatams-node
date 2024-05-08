5a8f8974c10649acc0650b30c838534e267c193d
bryllemutia <bryllemutia3@gmail.com>
2024-05-08 14:43:04 +0800

    added auth router

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { authRouter } from "./router/authRouter.js";
 
 dotenv.config();
 
-const PORT = 8000;
+const PORT = 8080;
 const MONGO_URI = `mongodb+srv://bryllemutia:${process.env.MONGO_PASSWORD}@cluster0.qt8cm1e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // you can explicitly annotate the types, but is not required
@@ -31,5 +32,7 @@ server.listen(PORT, () => {
 });
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI).then(() => console.log("Connected to DB!"));
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/auth", authRouter);
