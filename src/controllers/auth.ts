9e6ca5dac4e41d9f4dfd69a002990cc8cceade4a
bryllemutia <bryllemutia3@gmail.com>
2024-05-15 01:23:36 +0800

    feature: added get and delete for user controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -10,7 +10,10 @@ export const register = async (req: express.Request, res: express.Response) => {
 
       const existingUser = await UserModel.findOne({ email });
       if (existingUser)
-         return res.status(400).json({ message: "User already exists" }).end();
+         return res
+            .status(400)
+            .json({ message: "User already exists", id: existingUser._id })
+            .end();
 
       const salt = random();
       const user = await UserModel.build({
@@ -26,6 +29,46 @@ export const register = async (req: express.Request, res: express.Response) => {
       console.log(user.updated_at);
 
       return res.status(200).json(user).end();
+   } catch (err) {
+      console.log(err);
+      return res.sendStatus(400);
+   }
+};
+
+export const getUserDetails = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const userId = req.params.id;
+
+      const existingUser = await UserModel.findById(userId);
+      if (!existingUser)
+         return res.status(400).json({ message: "User doesn't exist" }).end();
+
+      return res.status(200).json(existingUser).end();
+   } catch (err) {
+      console.log(err);
+      return res.sendStatus(400);
+   }
+};
+
+export const deleteUser = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const userId = req.params.id;
+
+      if (!userId) return res.sendStatus(400);
+
+      const existingUser = await UserModel.findById(userId);
+      if (!existingUser)
+         return res.status(400).json({ message: "User doesn't exist" }).end();
+
+      const deletedUser = await UserModel.findByIdAndDelete(userId);
+
+      return res.status(200).json(deletedUser).end();
    } catch (err) {
       console.log(err);
       return res.sendStatus(400);
