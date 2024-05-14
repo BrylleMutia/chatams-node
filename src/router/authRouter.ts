1ae1b952bff3c4ca61e6d755ef2eec4c8debe27d
bryllemutia <bryllemutia3@gmail.com>
2024-05-15 01:24:04 +0800

    added get and delete user routes to auth router

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { register, deleteUser, getUserDetails } from "../controllers/auth.js";
 
 const router = express.Router();
 
+router.get("/user/:id", [], getUserDetails)
 router.post("/register", [], register);
+router.delete("/user/:id", [], deleteUser);
 
 export { router as authRouter };
