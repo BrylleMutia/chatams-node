b41601d6a236182700e53413c2189bf8df9c96dd
bryllemutia <bryllemutia3@gmail.com>
2024-05-16 13:54:07 +0800

    added role model

---
+++ src/models/role.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface IRole {
+   name: string;
+}
+
+// output interface from mongoose
+interface RoleDoc extends IRole, mongoose.Document {
+   createdAt: Date;
+   updatedAt: Date;
+}
+
+// Put all user instance methods in this interface (statics)
+interface RoleModelType extends mongoose.Model<IRole> {
+   build(attr: IRole): RoleDoc;
+}
+
+const roleSchema = new mongoose.Schema<IRole, RoleModelType>(
+   {
+      name: { type: String, required: true },
+   },
+   {
+      timestamps: true,
+   }
+);
+roleSchema.static("build", (attr: IRole) => new RoleModel(attr));
+
+export const RoleModel = mongoose.model<IRole, RoleModelType>(
+   "client",
+   roleSchema
+);
