78ad9fff942a9da399842920574bd9fb69b2ff23
bryllemutia <bryllemutia3@gmail.com>
2024-05-17 13:42:31 +0800

    added auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,19 +1,27 @@
 import express from "express";
 import { UserModel } from "../models/user.js";
 import { authentication, random } from "../helpers/index.js";
+import { ClientModel } from "../models/client.js";
+import mongoose from "mongoose";
 
 export const register = async (req: express.Request, res: express.Response) => {
    try {
-      const { email, username, password } = req.body;
+      const { email, username, password, client_id } = req.body;
 
       if (!email || !password || !username) return res.sendStatus(400);
 
+      // check if user email exists
       const existingUser = await UserModel.findOne({ email });
       if (existingUser)
          return res
             .status(400)
             .json({ message: "User already exists", id: existingUser._id })
             .end();
+
+      // check if client exists
+      const existingClient = await ClientModel.findById(client_id);
+      if (!existingClient)
+         return res.status(400).json({ message: "Client not found" }).end();
 
       const salt = random();
       const user = await UserModel.build({
@@ -23,15 +31,92 @@ export const register = async (req: express.Request, res: express.Response) => {
             password: authentication(salt, password),
             salt,
          },
+         clientId: client_id,
+         isApproved: true,
+         roleId: new mongoose.mongo.ObjectId("2"), // admin role id
       }).save();
 
-      console.log(user.created_at);
-      console.log(user.updated_at);
-
-      return res.status(200).json(user).end();
+      return res.status(200).json({ data: user }).end();
    } catch (err) {
       console.log(err);
-      return res.sendStatus(400);
+      return res
+         .status(400)
+         .json({ error: `User auth failed with error: ${err}` })
+         .end();
+   }
+};
+
+export const login = async (req: express.Request, res: express.Response) => {
+   try {
+      const { email, password } = req.body;
+
+      if (!email || !password) return res.sendStatus(400);
+
+      // check if user email exists
+      const existingUser = await UserModel.findOne({ email });
+      if (!existingUser)
+         return res
+            .status(400)
+            .json({ message: "Incorrect email or password" })
+            .end();
+
+      // check if password matched
+      const password_hash = authentication(
+         existingUser.authentication.salt,
+         password
+      );
+      if (existingUser.authentication.password !== password_hash)
+         return res
+            .status(400)
+            .json({ message: "Incorrect email or password" })
+            .end();
+
+      // populate client details
+      existingUser.populate("client");
+
+      return res.status(200).json({ data: existingUser }).end();
+   } catch (err) {
+      console.log(err);
+      return res
+         .status(400)
+         .json({ error: `User auth failed with error: ${err}` })
+         .end();
+   }
+};
+
+// handle approvals for new users
+export const toggleApprovals = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const { toggled_user_ids } = req.body;
+      const clientId = req.body.params.clientId;
+
+      if (toggled_user_ids.length < 1)
+         return res
+            .status(400)
+            .json({ message: "No user IDs to update approval" });
+
+      let updatedUserIds = [];
+      for (const userId of updatedUserIds) {
+         const user = await UserModel.findOne({
+            _id: userId,
+            clientId: clientId,
+         });
+         user.isApproved = !user.isApproved; // toggle
+         user.save();
+
+         updatedUserIds.push(user._id);
+      }
+
+      return res.status(200).json({ data: updatedUserIds }).end();
+   } catch (err) {
+      console.log(err);
+      return res
+         .status(400)
+         .json({ error: `User approval failed with error: ${err}` })
+         .end();
    }
 };
 
@@ -46,10 +131,38 @@ export const getUserDetails = async (
       if (!existingUser)
          return res.status(400).json({ message: "User doesn't exist" }).end();
 
-      return res.status(200).json(existingUser).end();
+      existingUser.populate("client");
+
+      return res.status(200).json({ data: existingUser }).end();
    } catch (err) {
       console.log(err);
-      return res.sendStatus(400);
+      return res
+         .status(400)
+         .json({ error: `Get user details failed with error: ${err}` });
+   }
+};
+
+export const getUsersInClient = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const clientId = req.params.clientId;
+
+      const existingUsers = await UserModel.find({ clientId });
+
+      if (existingUsers.length < 1)
+         return res
+            .status(400)
+            .json({ message: "No existing users for client" })
+            .end();
+
+      return res.status(200).json({ data: existingUsers }).end();
+   } catch (err) {
+      console.log(err);
+      return res
+         .status(400)
+         .json({ error: `Get users failed with error: ${err}` });
    }
 };
 
