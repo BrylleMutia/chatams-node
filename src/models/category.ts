127d741daf9600b662778924044663407eb09865
bryllemutia <bryllemutia3@gmail.com>
2024-05-17 12:29:17 +0800

    added category model

---
+++ src/models/category.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+interface ICategory {
+   name: string;
+   clientId: mongoose.Types.ObjectId;
+}
+
+// output interface from mongoose
+interface CategoryDoc extends ICategory, mongoose.Document {
+   createdAt: Date;
+   updatedAt: Date;
+}
+
+// Put all user instance methods in this interface (statics)
+interface CategoryModelType extends mongoose.Model<ICategory> {
+   build(attr: ICategory): CategoryDoc;
+}
+
+const categorySchema = new mongoose.Schema<ICategory, CategoryModelType>(
+   {
+      name: { type: String, required: true },
+      clientId: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: "Client" },
+   },
+   {
+      timestamps: true,
+   }
+);
+categorySchema.static("build", (attr: ICategory) => new CategoryModel(attr));
+
+export const CategoryModel = mongoose.model<ICategory, CategoryModelType>(
+   "Category",
+   categorySchema
+);
