4b08cdb9f280f3dfc37fe79e8abdb92167aa73bc
bryllemutia <bryllemutia3@gmail.com>
2024-05-27 21:26:21 +0800

    feat: initialize indexing for llm

--- src/helpers/initializeIndex.ts
+++ src/helpers/initializeIndex.ts
@@ -1,7 +1,19 @@
-import { SimpleDirectoryReader, VectorStoreIndex } from "llamaindex";
+import {
+   OpenAI,
+   Settings,
+   SimpleDirectoryReader,
+   VectorStoreIndex,
+} from "llamaindex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// initialize llm configuration
+Settings.llm = new OpenAI({ model: "gpt-4-32k", temperature: 0 });
 
 export const initializeIndex = async (clientName: string, category: string) => {
-   const directoryPath = `src/indexes/${clientName}/${category}`;
+   // const directoryPath = `src/indexes/${clientName}/${category}`;
+   const directoryPath = `src/indexes`;
 
    // load data from documents within directory
    const documents = await new SimpleDirectoryReader().loadData({
