d874bd9b135016feb721a1527e8452f82f32120b
bryllemutia <bryllemutia3@gmail.com>
2024-06-04 20:25:56 +0800

    added new getter routes for client + initialiazed index upon add client

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { UserModel } from "../models/user.js";
 import { ClientModel } from "../models/client.js";
+import { initializeCategoryInDB } from "../seeders/categorySeeder.js";
+import {
+   createCategoryIndex,
+   createClientDirectory,
+} from "../helpers/categoryIndexing.js";
 
 export const getUsersInClient = async (
    req: express.Request,
@@ -37,11 +42,52 @@ export const addNewClient = async (
          name: clientName.toUpperCase(),
       }).save();
 
+      // add new category records for client in db
+      initializeCategoryInDB(client.id).then(() => {
+         // create file structure for client + indexes
+         createClientDirectory(client.name).then(() => {
+            createCategoryIndex(client.id);
+         });
+      });
+
       return res.status(200).json({ data: client });
    } catch (err) {
       console.log(err);
       return res
          .status(400)
          .json({ error: `Add new client failed with error: ${err}` });
+   }
+};
+
+export const getClientDetails = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const clientId = req.params.clientId;
+      const client = await ClientModel.findById(clientId).exec();
+
+      return res.status(200).json({ data: client });
+   } catch (err) {
+      console.log(err);
+      return res
+         .status(400)
+         .json({ error: `Get client details failed with error: ${err}` });
+   }
+};
+
+export const getAllClientDetails = async (
+   req: express.Request,
+   res: express.Response
+) => {
+   try {
+      const client = await ClientModel.find({}).exec();
+
+      return res.status(200).json({ data: client });
+   } catch (err) {
+      console.log(err);
+      return res
+         .status(400)
+         .json({ error: `Get client details failed with error: ${err}` });
    }
 };
